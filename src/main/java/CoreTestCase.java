f7287985b1573126f55b0f90f224739fbf01a3e4
artur-arutsiunian <81415213+artur-arutsiunian@users.noreply.github.com>
2022-04-13 15:13:19 +0300

    добавил обработку нотификации на начальном экране(убрал ее)

--- src/main/java/CoreTestCase.java
+++ src/main/java/CoreTestCase.java
@@ -2,20 +2,28 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CoreTestCase {
 
 
     protected WebDriver driver;
+    protected ChromeOptions options;
     @Before
     public void SetUp(){
-        System.setProperty("webdriver.chrome.driver","/Users/arturarutsiunian/Documents/ZegoalWeb/src/main/resources/chromedriver");
+       System.setProperty("webdriver.chrome.driver","/Users/arturarutsiunian/Desktop/ZegoalWeb/src/main/resources/chromedriver");
 
-        driver = new ChromeDriver();
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+        options = new ChromeOptions();
+        options.setExperimentalOption("prefs", prefs);
+        driver = new ChromeDriver(options);
 
         driver.get("https://testing7.zgdev.info/auth");
         driver.manage().window().maximize();
-
     }
 
     @After
