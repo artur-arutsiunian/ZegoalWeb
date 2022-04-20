cf82f6c4f259899a4ddcdbd94a7674508db14016
artur-arutsiunian <81415213+artur-arutsiunian@users.noreply.github.com>
2022-04-20 12:01:12 +0300

    еще раз написал как можно можно использовать ассерты

--- src/test/java/tests/Task/EditTaskTest.java
+++ src/test/java/tests/Task/EditTaskTest.java
@@ -5,12 +5,15 @@ import lib.ui.Login.LoginPageObject;
 import lib.ui.Task.EditTaskPageObject;
 import lib.ui.Task.Filter.AdditionalFilterTaskPageObject;
 import lib.ui.Task.Filter.MainFilterTaskPageObject;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 
 public class EditTaskTest extends CoreTestCase {
 
     @Test
     public void testEditTask() throws InterruptedException {
+
         LoginPageObject LoginPageObject = new LoginPageObject(driver);
         LoginPageObject.loginMethod();
 
@@ -22,5 +25,7 @@ public class EditTaskTest extends CoreTestCase {
 
         EditTaskPageObject EditTaskPageObject = new EditTaskPageObject(driver);
         EditTaskPageObject.editTaskMethod();
+        String user_name = EditTaskPageObject.editTaskMethod2();
+        Assert.assertEquals("expected result not correspond actual","Brad Rouse", user_name);
     }
 }
